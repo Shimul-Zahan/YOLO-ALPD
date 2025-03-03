b8bf7dde96f4b7b3e65ee432517e697f79ab3138
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-03-03 14:02:32 +0600

    add some folder structure Commit #2:

---
+++ src/app.py
@@ -0,0 +1,10 @@
+# This is for web application
+
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+# import tensorflow
+
+print("Hello World")
